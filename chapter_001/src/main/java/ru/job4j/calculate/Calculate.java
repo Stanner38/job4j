8bebb009326134f0ddfc340c4791cea8f655f386
Stanner38 <mailto:stansyr13@gmail.com>
2018-09-18 14:03:03 +0300

    1.3. JavDoc. Code convention.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,16 @@
 package ru.job4j.calculate;
+/**
+@author Stanislav
+@since 18.09.2018
+@version 1
+*/
+
 public class Calculate {
+	/**
+	@Method main;
+	@Param args строка;
+	*/
+		
 	public static void main (String [] args){
 	System.out.println ("Hello world!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package для вычислительных заданий.
+*
+* @author Me
+* @version 1
+* @since 18.09.2018
+*/
+package ru.job4j.calculate;
