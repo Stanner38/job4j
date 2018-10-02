c352e23d0e04b1c45d343297fa91a53e1c5226f7
Stanner38 <mailto:stansyr13@gmail.com>
2018-10-02 21:46:08 +0300

    3.1. Элементарный калькулятор. [#185] Добавлены джавадоки.

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,24 +1,58 @@
 package ru.job4j.calculator;
 
+/**
+ * Class for calculator.
+ * @author Stanis.
+ * @version 1.
+ * @since 02.10.2018.
+ */
+
 public class Calculator {
+    /**
+     * variable of result.
+     */
     private double result;
-    
+
+    /**
+     * Method "add"  calculates a sum of two numbers.
+     * @param first first input number.
+     * @param second second input number.
+     */
     public void add(double first, double second) {
         this.result = first + second;
     }
-    
+
+    /**
+     * Method "subtract"  calculates a subtraction of two numbers.
+     * @param first first input number.
+     * @param second second input number.
+     */
 	public void subtract(double first, double second) {
         this.result = first - second;
     }
-	
+
+    /**
+     * Method "div"  calculates a dividing of two numbers.
+     * @param first first input number.
+     * @param second second input number.
+     */
 	public void div(double first, double second) {
         this.result = first / second;
     }
-	
+
+    /**
+     * Method "multiple"  calculates a multiplying of two numbers.
+     * @param first first input number.
+     * @param second second input number.
+     */
 	public void multiple(double first, double second) {
         this.result = first * second;
     }
-	
+
+    /**
+     *
+     * @return result of arithmetic operation.
+     */
     public double getResult() {
         return this.result;
     }
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculator.
+*
+* @author Stanis
+* @version 1
+* @since 18.09.2018
+*/
+package ru.job4j.calculator;
